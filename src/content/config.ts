90811b8a7cc78e25ec4788c5c15da20e156a28a1
Yanni Zhuang <35969859+zhuangy47@users.noreply.github.com>
2026-06-12 04:58:09 -0500

    new website! (#1)
    * new site
    
    * colours
    
    * new favicon
    
    * mobile header

---
+++ src/content/config.ts
@@ -0,0 +1,23 @@
+import { defineCollection, z } from 'astro:content';
+
+const blog = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    description: z.string(),
+    tags: z.array(z.string()).default([]),
+  }),
+});
+
+const dumping = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    url: z.string().url(),
+    thumbnail: z.string().optional(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = { blog, dumping };
